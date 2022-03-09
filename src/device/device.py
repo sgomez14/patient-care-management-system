6ec0b765e131a7935db07a5d8334fb5a12d936f8
sgomez14 <santi09@bu.edu>
2022-03-09 18:04:15 -0500

    Update device.py

--- src/device/device.py
+++ src/device/device.py
@@ -588,7 +588,7 @@ def is_device_registered(device_id: int, flask_path=False):  # -> List[bool, str
     database = "data/registered_devices.json"
 
     if flask_path:
-        database = os.path.join(flask_path, "patient-care-management-system",
+        database = os.path.join(flask_path, "Project2", "patient-care-management-system",
                                 "src", "device", "data", "registered_devices.json")
 
     # connect to database, which is a json at this stage in module development
